66f858a21f973e35d564425cc749652ce803f144
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-08-03 21:45:37 +0700

    feat: checking image service

--- app/services/mongoose/images.js
+++ app/services/mongoose/images.js
@@ -1,4 +1,5 @@
 const Images = require("../../api/v1/images/model");
+const { NotFound } = require("../../errors");
 
 const createImage = async (req) => {
     const result = await Images.create({
@@ -10,4 +11,13 @@ const createImage = async (req) => {
     return result;
 };
 
-module.exports = { createImage };
+const checkingImage = async (id) => {
+    const result = await Images.findById(id);
+    if (!result) {
+        throw new NotFound(`Tidak ada gambar dengan id: ${id}`);
+    }
+
+    return result;
+};
+
+module.exports = { createImage, checkingImage };
