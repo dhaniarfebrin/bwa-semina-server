83f77218c64d90917785b19b1a2d22b317df5e8b
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-08-04 14:30:39 +0700

    feat: event api service handler (controller) added

---
+++ app/api/v1/events/controller.js
@@ -0,0 +1,71 @@
+const {
+    getAllEvents,
+    getOneEvents,
+    updateEvent,
+    createEvents,
+    deleteEvents,
+} = require("../../../services/mongoose/events");
+
+const { StatusCodes } = require("http-status-codes");
+
+const create = async (req, res, next) => {
+    try {
+        const result = await createEvents(req);
+
+        res.status(StatusCodes.CREATED).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const index = async (req, res, next) => {
+    try {
+        const result = await getAllEvents(req);
+
+        res.status(StatusCodes.OK).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const find = async (req, res, next) => {
+    try {
+        const result = await getOneEvents(req);
+
+        res.status(StatusCodes.OK).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        const result = await updateEvent(req);
+
+        res.status(StatusCodes.OK).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const destroy = async (req, res, next) => {
+    try {
+        const result = await deleteEvents(req);
+
+        res.status(StatusCodes.OK).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = { create, index, find, update, destroy };
