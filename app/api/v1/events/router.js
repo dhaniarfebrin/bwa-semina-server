8c100ac577846164c3f628bea503f1c24786fbd4
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-08-08 11:01:24 +0700

    feat: middleware and role access

--- app/api/v1/events/router.js
+++ app/api/v1/events/router.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express();
 
+const {
+    authenticateUser,
+    authorizeRoles,
+} = require("../../../middleware/auth");
 const { create, index, find, update, destroy } = require("./controller");
 
-router.get("/events", index);
-router.get("/events/:id", find);
-router.post("/events", create);
-router.put("/events/:id", update);
-router.delete("/events/:id", destroy);
+router.get("/events", authenticateUser, authorizeRoles("organizer"), index);
+router.get("/events/:id", authenticateUser, authorizeRoles("organizer"), find);
+router.post("/events", authenticateUser, authorizeRoles("organizer"), create);
+router.put(
+    "/events/:id",
+    authenticateUser,
+    authorizeRoles("organizer"),
+    update
+);
+router.delete(
+    "/events/:id",
+    authenticateUser,
+    authorizeRoles("organizer"),
+    destroy
+);
 
 module.exports = router;
--- app/api/v1/talents/router.js
+++ app/api/v1/talents/router.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express();
 const { create, index, find, update, remove } = require("./controller");
 
-router.get("/talents", index);
-router.get("/talents/:id", find);
-router.put("/talents/:id", update);
-router.delete("/talents/:id", remove);
-router.post("/talents/", create);
+const {
+    authenticateUser,
+    authorizeRoles,
+} = require("../../../middleware/auth");
+
+router.get("/talents", authenticateUser, authorizeRoles("organizer"), index);
+router.get("/talents/:id", authenticateUser, authorizeRoles("organizer"), find);
+router.put(
+    "/talents/:id",
+    authenticateUser,
+    authorizeRoles("organizer"),
+    update
+);
+router.delete(
+    "/talents/:id",
+    authenticateUser,
+    authorizeRoles("organizer"),
+    remove
+);
+router.post("/talents/", authenticateUser, authorizeRoles("organizer"), create);
 
 module.exports = router;
