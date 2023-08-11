f3d834f64466fcd5f6622f24b6bbf48933751750
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-08-12 00:01:12 +0700

    feat: get all event as participant

--- app/api/v1/participants/router.js
+++ app/api/v1/participants/router.js
@@ -5,10 +5,17 @@ const router = express();
 //     authenticateUser,
 //     authorizeRoles,
 // } = require("../../../middleware/auth");
-const { signin, signup, activeParticipant } = require("./controller");
+const {
+    signin,
+    signup,
+    activeParticipant,
+    getAllLandingPage,
+} = require("./controller");
 
 router.post("/auth/signup", signup);
 router.post("/auth/signin", signin);
 router.put("/auth/active", activeParticipant);
+
+router.get("/events", getAllLandingPage);
 
 module.exports = router;
